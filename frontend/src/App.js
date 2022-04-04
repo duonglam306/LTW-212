3239a69370bf87229b5861d3861b97782ebd3499
duonglam306 <lamduong11201@gmail.com>
2022-04-04 08:02:36 +0700

    Create frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './image/logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Chào mọi người đã đến với cửa hàng Hola Housè</h1>
+      <img src={logo} alt="main-logo" />
+    </div>
+  );
+}
+
+export default App;
